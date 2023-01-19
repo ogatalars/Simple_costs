8b778b8648f7419ece8d5b9bd3112551778c6a64
ogatalars <ogataogara@gmail.com>
2023-01-19 15:38:24 -0300

    using some logic to implement props

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -5,13 +5,26 @@ function ExpenseItem(props) {
   // const expenseDate = new Date(2023, 3, 23);
   // const expenseTitle = "Car Insurence";
   // const expenseAmount = 245.32;
-
+  const month = props.date.toLocalString("en-US", { month: "long" });
+  const day = props.date.toLocalString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
   return (
+    // <div className="expense-item">
+    //   <div>{props.date.toISOString()}</div>
+    //   <div className="expense-item__description">
+    //     <h2>{props.title} </h2>
+    //     <div className="expense-item__price">{props.amount}</div>
+    //   </div>
+    // </div>
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
-        <h2>{props.title} </h2>
-        <div className="expense-item__price">{props.amount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price"> ${props.amount}</div>
       </div>
     </div>
   );
