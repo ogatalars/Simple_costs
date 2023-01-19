b6a2d60180dad1fdbce72eaf4dcf81c0d3369ec2
ogatalars <ogataogara@gmail.com>
2023-01-19 16:13:11 -0300

    Add one more component just for training'

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,3 +1,4 @@
+import ExpenseDate from "./ExpenseDate";
 // a component in react is just a js function
 import "./Expenseitem.css";
 
@@ -5,9 +6,9 @@ function ExpenseItem(props) {
   // const expenseDate = new Date(2023, 3, 23);
   // const expenseTitle = "Car Insurence";
   // const expenseAmount = 245.32;
-  const month = props.date.toLocalString("en-US", { month: "long" });
-  const day = props.date.toLocalString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
+  // const month = props.date.toLocalString("en-US", { month: "long" });
+  // const day = props.date.toLocalString("en-US", { day: "2-digit" });
+  // const year = props.date.getFullYear();
   return (
     // <div className="expense-item">
     //   <div>{props.date.toISOString()}</div>
@@ -17,11 +18,12 @@ function ExpenseItem(props) {
     //   </div>
     // </div>
     <div className="expense-item">
-      <div>
+      <ExpenseDate date={props.date} />
+      {/* <div>
         <div>{month}</div>
         <div>{year}</div>
         <div>{day}</div>
-      </div>
+      </div> */}
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price"> ${props.amount}</div>
