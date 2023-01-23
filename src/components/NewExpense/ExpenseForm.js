f270ecd081210207cff9bf61e2d6ce99ceab11cd
ogatalars <ogataogara@gmail.com>
2023-01-23 16:31:11 -0300

    adding some structure for forms

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,20 @@
+import "./ExpenseForm.css";
+const ExpenseForm = () => {
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control"> </div>
+        <label>Title</label>
+        <input type="text" />
+        <div className="new-expense__control"> </div>
+        <label>Amount</label>
+        <input type="number" />
+        <div className="new-expense__control"> </div>
+        <label>Date </label>
+        <input type="date" />
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
