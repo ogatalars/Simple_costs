d1ef77eb50cc7b23b3b01ff309607394c926ebba
ogatalars <ogataogara@gmail.com>
2023-02-10 14:54:14 -0300

    fix: Major problems. Draft

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,15 +1,16 @@
-import { useState } from "react";
-import "./Expenses.css";
+import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
 import ExpensesChart from "./ExpensesChart";
+import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2023");
+  const [filteredYear, setFilteredYear] = useState("2020");
+
   const filterChangeHandler = (selectedYear) => {
-    console.log(selectedYear);
+    setFilteredYear(selectedYear);
   };
 
   const filteredExpenses = props.items.filter((expense) => {
